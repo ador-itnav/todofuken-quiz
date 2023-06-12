a9abf3544fac0ff30819eeb1f33f3b4d48bf0bf1
ador-itnav <donquijote0103@gmail.com>
2023-06-12 13:36:59 +0900

    /commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/github-pages-test/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/todofuken-quiz/' : './',
   plugins: [vue()],
 });
