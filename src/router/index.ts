d830a270e39293b24e4daaccaa72ea89024d0628
ador-itnav <donquijote0103@gmail.com>
2023-06-12 15:15:27 +0900

    /commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: './',
+    path: '/src',
     name: 'App',
     component: () => import('../App.vue'),
   },
