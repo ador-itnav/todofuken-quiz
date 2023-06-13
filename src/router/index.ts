d0f8aa16da9c07332b4c1c4dc34e4b1a06a16685
ador-itnav <donquijote0103@gmail.com>
2023-06-13 09:20:08 +0900

    /commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,7 +10,7 @@ const routes: Array<RouteRecordRaw> = [
 
 const router = createRouter({
   // Viteの環境変数でimport.meta.env.BASE_URL = vite.config.tsのbase
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
